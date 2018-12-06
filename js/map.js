e3b042c942a4a82c0e246bb4218d86501335bf9d
Alexander Strakhovenko <sasstr@gmail.com>
2018-12-06 21:19:52 +0300

    Внес правки после первой проверки

--- js/map.js
+++ js/map.js
@@ -24,6 +24,8 @@ var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http:
 var ENABLED_CONDITION = false;
 var DISABLED_CONDITION = true;
 var ESC_KEYCODE = 27;
+var ROOMS = {'1': {'1': 'для 1 гостя'}, '2': {'1': 'для 1 гостя', '2': 'для 2 гостей'}, '3': {'1': 'для 1 гостя', '2': 'для 2 гостей', '3': 'для 3 гостей'}, '100': {'0': 'не для гостей'}};
+var TYPES_OF_HABITATION = {'bungalo': 0, 'flat': 1000, 'house': 5000, 'palace': 10000};
 
 var map = document.querySelector('.map');
 
@@ -231,11 +233,12 @@ var mainPinMouseupHandler = function (evt) {
   map.classList.remove('map--faded');
   adForm.classList.remove('ad-form--disabled');
   setConditionForms(ENABLED_CONDITION);
+  enableForm();
   mapPins.appendChild(renderPins());
   inputAddress.value = getCoordinatesOfMainPin(evt);
   mapPinMain.removeEventListener('mouseup', mainPinMouseupHandler);
 };
-
+// Функция, которая переводит страницу в начальное состояние. Реагирует только маффин на перетаскивание мышкой
 var disableForm = function () {
   setConditionForms(DISABLED_CONDITION);
   mapPinMain.addEventListener('mouseup', mainPinMouseupHandler);
@@ -245,58 +248,57 @@ disableForm();
 
 var roomNumber = document.querySelector('#room_number');
 var capacity = document.querySelector('#capacity');
-
-var rooms = {
-  '1': {'1': 'для 1 гостя'},
-  '2': {'1': 'для 1 гостя', '2': 'для 2 гостей'},
-  '3': {'1': 'для 1 гостя', '2': 'для 2 гостей', '3': 'для 3 гостей'},
-  '100': {'0': 'не для гостей'},
-};
-// Функция собирает список select на основе значений option другого select
-var selectRoomsChangeHandler = function () {
+// Функция создает список select с кол-ом гостей в соответсвии с кол-ом комнат
+var getRightNumberOfGuests = function () {
   capacity.innerHTML = '';
   var roomCount = roomNumber.options[roomNumber.selectedIndex].value;
-  var room = rooms[roomCount];
+  var room = ROOMS[roomCount];
   var keys = Object.keys(room);
   for (var k = 0; k < keys.length; k++) {
     var value = keys[k];
     var valueString = room[keys[k]];
-    var options = new Option(valueString, value, false, false);
-    capacity.add(options);
+    var option = new Option(valueString, value, false, false);
+    capacity.add(option);
   }
 };
 
-roomNumber.addEventListener('change', selectRoomsChangeHandler);
-
-var typeOfDwelling = {
-  'bungalo': 0,
-  'flat': 1000,
-  'house': 5000,
-  'palace': 10000
+// Функция собирает список select на основе значений option другого select
+var selectRoomsChangeHandler = function () {
+  getRightNumberOfGuests();
 };
 
 var typeOfHabitation = document.querySelector('#type');
 var inputMinMaxPrice = document.querySelector('#price');
 
-// Функция вставляет нужное значение минимальной стоимости жилья.
-var inputTypeChangeHandler = function () {
-  inputMinMaxPrice.min = typeOfDwelling[typeOfHabitation.value];
+// Функция вставляет в разметку минмальную стоимость жилья
+var setRightMinPriceOfDwelling = function () {
+  inputMinMaxPrice.min = TYPES_OF_HABITATION[typeOfHabitation.value];
 };
 
-typeOfHabitation.addEventListener('change', inputTypeChangeHandler);
+// Функция слушатель события выбора типа жилья, которая вставляет нужное значение минимальной стоимости жилья.
+var inputTypeChangeHandler = function () {
+  setRightMinPriceOfDwelling();
+};
 
 var selectTimein = document.querySelector('#timein');
 var selectTimeout = document.querySelector('#timeout');
 
-
+// Функция синхронизации времени въезда и отъезда гостя
 var selectTimeinChangeHandler = function () {
   selectTimeout.selectedIndex = selectTimein.selectedIndex;
 };
 
-selectTimein.addEventListener('change', selectTimeinChangeHandler);
-
+// Функция синхронизации времени въезда и отъезда гостя
 var selectTimeoutChangeHandler = function () {
   selectTimein.selectedIndex = selectTimeout.selectedIndex;
 };
 
-selectTimeout.addEventListener('change', selectTimeoutChangeHandler);
+// Функция установки начального состояния
+var enableForm = function () {
+  getRightNumberOfGuests();
+  setRightMinPriceOfDwelling();
+  roomNumber.addEventListener('change', selectRoomsChangeHandler);
+  typeOfHabitation.addEventListener('change', inputTypeChangeHandler);
+  selectTimeout.addEventListener('change', selectTimeoutChangeHandler);
+  selectTimein.addEventListener('change', selectTimeinChangeHandler);
+};
